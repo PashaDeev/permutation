f5b75e0b7717520053c806b0095ea9bec9c81804
Deev Pavel <deev@uchi.ru>
2019-10-20 17:53:36 +0300

    permutations

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-const test = [1, 2, 3];
+function make(arr) {
+  if (arr.length > 1) {
+    const start = arr[0];
+    const firstArr = make(arr.slice(1));
+    const secondArr = [];
+    const { length } = firstArr[0];
 
-const combination = (arr) => {
+    for (let i = 0; i < firstArr.length; i++) {
+      for (let j = 0; j <= length; j++) {
+        const first = firstArr[i].slice(0, j);
+        const second = firstArr[i].slice(j);
+        secondArr.push([...first, start, ...second]);
+      }
+    }
 
+    return secondArr;
+  } else return [arr];
+}
+
+const combination = length => {
+  if (length === 0) return [];
+  if (length === 1) return [[1]];
+
+  const arr = Array.from({ length: length }).map((item, index) => index + 1);
+
+  return make(arr);
 };
 
 console.log(`-------------------------`);
-console.log(`result`, combination(test));
+console.log(`comb`, combination(3));
 console.log(`-------------------------`);
+
