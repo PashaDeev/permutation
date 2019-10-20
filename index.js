283d6214329bcb405435905da841ee6e2645c9b6
Deev Pavel <deev@uchi.ru>
2019-10-20 16:33:30 +0300

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const test = [1, 2, 3];
+
+const combination = (arr) => {
+
+};
+
+console.log(`-------------------------`);
+console.log(`result`, combination(test));
+console.log(`-------------------------`);
